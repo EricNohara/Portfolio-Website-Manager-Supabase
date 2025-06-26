3e632b1c65b2539bb34027b4420d5a206bb30129
Eric Nohara-LeClair <Eric.NoharaLeClair@dotfoods.com>
2025-06-26 11:36:17 -0500

    Add favicon and edit website title

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Container } from "@mui/material";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export const metadata = {
-  title: "Portfolio Website Editor",
+  title: "Portfolio Manager",
   description: "Manager for portfolio website information and file storage",
 };
 
@@ -17,6 +17,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/icons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>
         <AuthProvider>
           <LocalizationProviderWrapper>
